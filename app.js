582193fd907fa2ba9d41fbdb737b998e07a24392
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-11 23:07:25 +0530

    style: final touch to the app

--- app.js
+++ app.js
@@ -15,28 +15,25 @@ function calculateProfitAndLoss(initial, quantity, current) {
   if (initial > current) {
     let loss = (initial - current) * quantity;
     let lossPercentage = (loss / (initial * quantity)) * 100;
-
+    let color = "red";
     showOutput(
-      `Your Loss is ${loss} and Loss Percentage is ${lossPercentage.toFixed(
-        2
-      )}%`
+      `Your Loss is ${loss} and Loss Percentage is ${lossPercentage.toFixed()}%`,
+      color
     );
-    // outputEl.innerText = `Your Loss is ${loss} and Loss Percentage is ${lossPercentage.toFixed(
-    //   2
-    // )}%`;
   } else if (current > initial) {
     let profit = (current - initial) * quantity;
     let profitPercentage = (profit / (initial * quantity)) * 100;
+    let color = "green";
     showOutput(
-      `Your Profit is ${profit} and Loss Percentage is ${profitPercentage.toFixed(
-        2
-      )}%`
+      `Your Profit is ${profit} and Loss Percentage is ${profitPercentage.toFixed()}%`,
+      color
     );
   } else {
     showOutput(`Well You Have Nothing To 'LOOSE' or 'GAIN'`);
   }
 }
-function showOutput(msg) {
+function showOutput(msg, color) {
   outputEl.innerText = msg;
+  outputEl.style.color = color;
 }
 submitButton.addEventListener("click", clickHandler);
